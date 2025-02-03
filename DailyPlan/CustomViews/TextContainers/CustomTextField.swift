dc0ac8a7265b807f617a4a0e8b10f9b82037c79f
Malik <timurkaev_malik@icloud.com>
2025-02-03 11:08:11 +0300

    Foldered

---
+++ DailyPlan/CustomViews/TextContainers/CustomTextField.swift
@@ -0,0 +1,83 @@
+//
+//  CustomTextField.swift
+//  DailyPlan
+//
+//  Created by Malik Timurkaev on 02.02.2025.
+//
+
+import SwiftUI
+
+///TODO: add method - (isFocused = false on screen tap)
+struct CustomTextField: View {
+    
+    @Binding private var text: String
+    @State private var lastText: String
+    private let color: Color
+    private var isFocused: FocusState<Bool>.Binding
+    
+    init(text: Binding<String>,
+         isFocused: FocusState<Bool>.Binding,
+         color: Color) {
+        self._text = text
+        self.color = color
+        self.isFocused = isFocused
+        lastText = ""
+    }
+    
+    var body: some View {
+        HStack(spacing: 0) {
+            TextField("", text: $text)
+                .focused(isFocused)
+                .tint(color)
+                .font(.taskText)
+                .frame(height: 60)
+                .padding(.leading, 10)
+                .placeHolder(present: isFocused.wrappedValue || !text.isEmpty) {
+                    Text("Category")
+                        .foregroundStyle(.gray)
+                        .font(.taskText)
+                        .padding(.horizontal, 10)
+                }
+            
+            clearButton()
+                .padding(.trailing, 10)
+        }
+        .clipped()
+        .overlay {
+            RoundedRectangle(cornerRadius: 16)
+                .stroke(color)
+        }
+        .onChange(of: text) {
+            if text.count > 25 {
+                text = lastText
+            } else {
+                lastText = text
+            }
+        }
+    }
+}
+
+private extension CustomTextField {
+    func clearButton() -> some View {
+        Button {
+            if isFocused.wrappedValue {
+                text = ""
+                isFocused.wrappedValue.toggle()
+            } else {
+                isFocused.wrappedValue = true
+            }
+        } label: {
+            Image(systemName: "x.square")
+                .foregroundStyle(isFocused.wrappedValue ? .messGrayText : .clear)
+                .font(.system(size: 20, weight: .medium))
+                .frame(width: 40, height: 40)
+        }
+    }
+}
+
+#Preview {
+    @FocusState var isFocused: Bool
+    return CustomTextField(text: .constant(""),
+                           isFocused: $isFocused,
+                           color: .ypWarmYellow)
+}
---
+++ DailyPlan/CustomViews/TextContainers/CustomTextView.swift
@@ -0,0 +1,161 @@
+//
+//  CustomTextView.swift
+//  DailyPlan
+//
+//  Created by Malik Timurkaev on 20.08.2024.
+//
+
+import SwiftUI
+
+struct CustomTextView: View {
+    
+    @FocusState private var isFocused: Bool
+    
+    @Binding private var text: String
+    @State private var lastText: String
+    
+    @State private var state: EditorState
+    @State private var stateValues: StateValues
+    
+    private let focusedHeight: FocusedHeight
+    private let color: Color
+    private let placeHolder: String
+    
+    init(text: Binding<String>,
+         color: Color,
+         focusedHeight: FocusedHeight,
+         placeHolder: String) {
+        self._text = text
+        self.color = color
+        self.placeHolder = placeHolder
+        self.focusedHeight = focusedHeight
+        
+        state = .default
+        lastText = ""
+        stateValues = StateValues(state: .default)
+    }
+    
+    var body: some View {
+        
+        RepresentedTextView(text: $text,
+                            placeHolder: placeHolder,
+                            linesNumber: focusedHeight == .medium ? 1 : 5)
+        .tint(color)
+        .focused($isFocused)
+        .frame(height: state == .focused ? focusedHeight.rawValue : 60)
+        .padding(.horizontal, 10)
+        .multilineTextAlignment(.leading)
+        .overlay {
+            ZStack(alignment: Alignment(horizontal: .trailing, vertical: .bottom)) {
+                
+                Color.clear
+                
+                HStack {
+                    cancelButton()
+                    confirmButton()
+                }
+                .padding(.trailing, 12)
+                .padding(.bottom, stateValues.padding)
+            }
+            .clipped()
+            .overlay(content: {
+                RoundedRectangle(cornerRadius: 16)
+                    .stroke(color)
+            })
+        }
+        .onChange(of: isFocused, {
+            lastText = text
+            switchStateWithAnimation()
+        })
+    }
+}
+
+extension CustomTextView {
+    enum FocusedHeight: CGFloat {
+        case medium = 84
+        case large = 184
+    }
+}
+
+private extension CustomTextView {
+    enum EditorState {
+        case focused
+        case `default`
+    }
+    
+    struct StateValues {
+        let padding: CGFloat
+        let height: CGFloat
+        
+        init(state: EditorState) {
+            switch state {
+            case .focused:
+                padding = 6
+                height = 30
+            case .default:
+                padding = 0
+                height = 0
+            }
+        }
+    }
+    
+    func cancelButton() -> some View {
+        Button {
+            text = lastText
+            isFocused = false
+        } label: {
+            Image(systemName: "multiply")
+                .frame(width: 50, height: stateValues.height)
+                .foregroundStyle(.ypRed)
+                .background(.messGradientFirst)
+                .clipShape(.buttonBorder)
+        }
+    }
+    
+    func confirmButton() -> some View {
+        Button {
+            isFocused = false
+        } label: {
+            Image(systemName: "checkmark")
+                .frame(width: 50, height: stateValues.height)
+                .foregroundStyle(.ypLightGreen)
+                .background(.messGradientBottom)
+                .clipShape(.buttonBorder)
+        }
+    }
+}
+
+private extension CustomTextView {
+    func switchStateWithAnimation() {
+        if state == .default {
+            
+            withAnimation {
+                state = .focused
+            } completion: {
+                withAnimation {
+                    stateValues = StateValues(state: state)
+                }
+            }
+        } else {
+            
+            let defaultState = EditorState.default
+            
+            withAnimation(.linear(duration: 0.2)) {
+                
+                stateValues = StateValues(state: defaultState)
+                
+            } completion: {
+                withAnimation {
+                    state = defaultState
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    CustomTextView(text: .constant(""),
+                   color: .ypWarmYellow,
+                   focusedHeight: .large,
+                   placeHolder: "Description")
+}
