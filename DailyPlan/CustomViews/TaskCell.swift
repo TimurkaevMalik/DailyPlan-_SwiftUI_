35c7a6c8e9d796b3cb894d3628d773138be2e51f
Malik <timurkaev_malik@icloud.com>
2025-02-21 19:08:38 +0300

    implemented DragGesture for TaskCell

--- DailyPlan/CustomViews/TaskCell.swift
+++ DailyPlan/CustomViews/TaskCell.swift
@@ -9,10 +9,19 @@ import SwiftUI
 
 struct TaskCell: View {
     
-    @State var task: TaskInfo
+    @State private var task: TaskInfo
+    @State private var descriptionViewOffSet: CGSize
+    @State private var padding: CGFloat
+    @FocusState private var isFocused: Bool
+    
+    init(task: TaskInfo) {
+        self.task = task
+        descriptionViewOffSet = .zero
+        padding = 6
+    }
     
     var body: some View {
-        HStack(alignment: .bottom, spacing: 6) {
+        HStack(alignment: .bottom, spacing: padding) {
             
             CheckMarkButton(color: task.color,
                             isDone: $task.isDone)
@@ -23,7 +32,54 @@ struct TaskCell: View {
                             focusedHeight: .large,
                             placeHolder: "Description",
                             schedule: task.schedule)
+            .focused($isFocused)
+            .offset(getOffSetAmount())
+            .onTapGesture {
+                
+            }
+            .gesture(dragGesture)
+            .onChange(of: isFocused) {
+                if isFocused {
+                    withAnimation {
+                        descriptionViewOffSet = .zero
+                    }
+                }
+            }
         }
+    }
+    
+    func getOffSetAmount() -> CGSize {
+        let minWidth = 0.0
+        let maxWidth = -(CGSize.checkMarkButton.width + padding)
+        let currentWidth = descriptionViewOffSet.width
+        
+        return CGSize(width: min(minWidth, max(maxWidth, currentWidth)), height: 0)
+    }
+}
+
+private extension TaskCell {
+    var dragGesture: some Gesture {
+        DragGesture(minimumDistance: 20, coordinateSpace: .local)
+            .onChanged { value in
+                if !isFocused {
+                    withAnimation {
+                        descriptionViewOffSet.width = value.translation.width
+                    }
+                }
+            }
+            .onEnded { value in
+                if !isFocused {
+                    if value.translation.width > -14 {
+                        withAnimation {
+                            descriptionViewOffSet = .zero
+                        }
+                    } else {
+                        withAnimation {
+                            descriptionViewOffSet.width = -(CGSize.checkMarkButton.width + padding)
+                        }
+                    }
+                }
+            }
     }
 }
 
