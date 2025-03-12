31a2506677f939ed59d23d6bc947731751efebb6
Malik <timurkaev_malik@icloud.com>
2025-03-12 20:20:14 +0300

    added hexString method

--- DailyPlan/Helpers/Extensions/Color.swift
+++ DailyPlan/Helpers/Extensions/Color.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 extension Color {
-    init(hex: String) {
+    init?(hex: String) {
         let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)//
         
         let a, r, g, b: UInt64 // UInt64?
@@ -29,13 +29,13 @@ extension Color {
                             rgbValue >> 8 & 0xFF,
                             rgbValue & 0xFF)
             
-        case 9: // ARGB (32-bit)
+        case 9: // RGBA (32-bit)
             (a, r, g, b) = (rgbValue >> 24,
                             rgbValue >> 16 & 0xFF,
                             rgbValue >> 8 & 0xFF,
                             rgbValue & 0xFF)
         default:
-            (a, r, g, b) = (1, 1, 1, 0)
+            return nil
         }
         
         self.init(.sRGB,
@@ -43,5 +43,41 @@ extension Color {
                   green: Double(g) / 255,
                   blue: Double(b) / 255,
                   opacity: Double(a) / 255)
+    }
+    
+    func hexString() -> String? {
+        
+        guard let components = UIColor(self).cgColor.components else {
+            return nil
+        }
+        
+        let r, g, b, a: CGFloat
+                
+        switch components.count {
+        case 2: //Gray Scale
+            (r, g, b, a) = (components[0],
+                            components[0],
+                            components[0],
+                            components[1])
+        case 3: //RGB
+            (r, g, b, a) = (components[0],
+                            components[1],
+                            components[2],
+                            1.0)
+        case 4: //RGBA
+            (r, g, b, a) = (components[0],
+                            components[1],
+                            components[2],
+                            components[3])
+        default:
+            return nil
+        }
+        
+        let red = String(format: "%02X", Int(r * 255))
+        let green = String(format: "%02X", Int(g * 255))
+        let blue = String(format: "%02X", Int(b * 255))
+        let alpha = String(format: "%02X", Int(a * 255))
+        
+        return "#\(red)\(green)\(blue)\(alpha)"
     }
 }
